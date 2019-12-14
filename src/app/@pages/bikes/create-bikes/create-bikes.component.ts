1dcb35eaa278aaa5d3f9bc4bd208c2f2a5aa0dfa
Ruslan Gonzalez <ruslanguns@gmail.com>
2019-12-14 17:23:52 +0000

    feat(bikes): go backward with ESC keyup

--- src/app/@pages/bikes/create-bikes/create-bikes.component.ts
+++ src/app/@pages/bikes/create-bikes/create-bikes.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, fromEvent } from 'rxjs';
+import { pluck, filter } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-bikes',
   templateUrl: './create-bikes.component.html',
   styleUrls: ['./create-bikes.component.css']
 })
-export class CreateBikesComponent implements OnInit {
+export class CreateBikesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private OnKeyEscapeClose: Subscription;
+  keyup$ = (keyCode: string) => {
+    return fromEvent<KeyboardEvent>(document, 'keyup')
+      .pipe(
+        pluck('key'),
+        filter(val => val === keyCode)
+      );
+  }
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.OnKeyEscapeClose = this.keyup$('Escape')
+      .subscribe(res => this.router.navigate(['/admin/bicicletas']));
+  }
+
+  ngOnDestroy() {
+    this.OnKeyEscapeClose.unsubscribe();
   }
 
 }
--- src/app/@pages/bikes/detail-bikes/detail-bikes.component.ts
+++ src/app/@pages/bikes/detail-bikes/detail-bikes.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { fromEvent } from 'rxjs/internal/observable/fromEvent';
+import { pluck, filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-detail-bikes',
   templateUrl: './detail-bikes.component.html',
   styleUrls: ['./detail-bikes.component.css']
 })
-export class DetailBikesComponent implements OnInit {
+export class DetailBikesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
 
-  ngOnInit() {
+  private OnKeyEscapeClose: Subscription;
+  keyup$ = (keyCode: string) => {
+    return fromEvent<KeyboardEvent>(document, 'keyup')
+      .pipe(
+        pluck('key'),
+        filter(val => val === keyCode)
+      );
   }
 
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.OnKeyEscapeClose = this.keyup$('Escape')
+      .subscribe(res => this.router.navigate(['/admin/bicicletas']));
+  }
+
+  ngOnDestroy() {
+    this.OnKeyEscapeClose.unsubscribe();
+  }
 }
