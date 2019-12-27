1fe1eb7ca2a385bada69fcb914a85f019c8bcd0a
Ruslan Gonzalez <ruslanguns@gmail.com>
2019-12-27 15:45:25 +0000

    fix(webpack): fix with context map webpack plugin

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,15 +1,15 @@
 const webpack = require("webpack");
 const WebpackConfigFactory = require("@nestjs/ng-universal")
   .WebpackConfigFactory;
-
 const nodeExternals = require("webpack-node-externals");
+const ContextMapPlugin = require('context-map-webpack-plugin');
 
 const webpackOptions = WebpackConfigFactory.create(webpack, {
   // This is our Nest server for Dynamic universal
   server: "./server/main.ts"
 });
 
-const whitelistedPackages = /^(?!(livereload|concurrently|mongoose|sharp|fastify-swagger)).*/;
+const whitelistedPackages = /^(?!(livereload|concurrently|mongoose|sharp|fastify-swagger|handlebars|UglifyJS)).*/;
 webpackOptions.externals[1] = nodeExternals({
   whitelist: whitelistedPackages
 });
@@ -19,12 +19,13 @@ webpackOptions.plugins.push(
     checkResource(resource) {
       const lazyImports = [
         "@nestjs/swagger",
+        "@nest-modules/mailer",
         "@nestjs/microservices",
         "@nestjs/microservices/microservices-module",
         "@nestjs/websockets/socket-module",
         "cache-manager",
         "class-validator",
-        "class-transformer"
+        "class-transformer",
       ];
       if (!lazyImports.includes(resource)) {
         return false;
@@ -36,7 +37,21 @@ webpackOptions.plugins.push(
       }
       return false;
     }
-  })
+  }),
+  //https://github.com/bastidest/context-map-webpack-plugin
+  new ContextMapPlugin('node_modules/uglify-js/tools', [
+    '../lib/utils.js',
+    '../lib/ast.js',
+    '../lib/parse.js',
+    '../lib/transform.js',
+    '../lib/scope.js',
+    '../lib/output.js',
+    '../lib/compress.js',
+    '../lib/sourcemap.js',
+    '../lib/mozilla-ast.js',
+    '../lib/propmangle.js',
+    './exports.js',
+  ])
 );
 
 module.exports = webpackOptions;
